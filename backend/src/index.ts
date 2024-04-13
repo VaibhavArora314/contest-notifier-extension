ee07c5553de00912917fe695c60b1e5b19a1173c
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-04-13 18:14:10 +0530

    revert changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,6 @@
 import app from "./app";
-import { UpdateContests } from "./store";
 const PORT = 3000;
-const RefreshIntervalMs = 12 * 60 * 60 * 1000; // refresh data every 12 hours
 
 app.listen(PORT, () => {
     console.log(`Server is running.`);
-    UpdateContests();
-
-    setTimeout(UpdateContests,RefreshIntervalMs);
 })
