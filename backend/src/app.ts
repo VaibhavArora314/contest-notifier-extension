bb459bca14ed0aef23cb52a16f17d3b88e59464c
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-04-15 18:03:18 +0530

    added platforms query param to only send required contests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import rootRouter from "./routes";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 app.use('/api', rootRouter);
 
--- backend/src/routes/contest/contest.controller.ts
+++ backend/src/routes/contest/contest.controller.ts
@@ -2,14 +2,34 @@ import { Request, Response } from "express";
 import getCodeforcesContests from "../../platforms/codeforces";
 import getLeetcodeContests from "../../platforms/leetcode";
 import getCodechefContests from "../../platforms/codechef";
+import { CONTEST_INTERFACE, PLATFORM } from "../../types";
 
 export const UpcomingContestsController = async (
   req: Request,
   res: Response
 ) => {
-  const codechefContests = await getCodechefContests();
-  const leetcodeContests = await getLeetcodeContests();
-  const codeforcesContests = await getCodeforcesContests();
+  let codeforcesContests: CONTEST_INTERFACE[] = [],
+    leetcodeContests: CONTEST_INTERFACE[] = [],
+    codechefContests: CONTEST_INTERFACE[] = [],
+    platforms: string[];
+  try {
+    platforms = JSON.parse(req.query.platforms as string);
+    if (!platforms || !Array.isArray(platforms))
+      platforms = [PLATFORM.CODECHEF, PLATFORM.CODEFORCES, PLATFORM.LEETCODE];
+
+    if (platforms.includes(PLATFORM.CODECHEF))
+      codechefContests = await getCodechefContests();
+    if (platforms.includes(PLATFORM.LEETCODE))
+      leetcodeContests = await getLeetcodeContests();
+    if (platforms.includes(PLATFORM.CODEFORCES))
+      codeforcesContests = await getCodeforcesContests();
+  } catch (error) {
+    platforms = [PLATFORM.CODECHEF, PLATFORM.CODEFORCES, PLATFORM.LEETCODE];
+
+    codechefContests = await getCodechefContests();
+    leetcodeContests = await getLeetcodeContests();
+    codeforcesContests = await getCodeforcesContests();
+  }
 
   const contests = [
     ...codechefContests,
@@ -22,6 +42,7 @@ export const UpcomingContestsController = async (
   );
 
   res.status(200).json({
+    platforms,
     upcoming_contests: contests,
   });
 };
