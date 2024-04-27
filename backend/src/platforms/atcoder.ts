536fa3c18e35d4d0f3df72649b2309b2b2534cba
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-04-27 18:55:21 +0530

    added atcoder current active contests and duration limit on contests to remove practise contests

--- backend/src/platforms/atcoder.ts
+++ backend/src/platforms/atcoder.ts
@@ -5,45 +5,53 @@ import { CONTEST_INTERFACE, PLATFORM } from "../types";
 const ATCODER_BASE_URL = "https://atcoder.jp"; // for generating contest url
 const ATCODER_CONTESTS_PAGE = "https://atcoder.jp/contests/";
 
+const parseTable = ($:cheerio.CheerioAPI,tbody:cheerio.Cheerio<cheerio.Element>) => {
+    const contests: CONTEST_INTERFACE[] = [];
+
+    tbody.find('tr').each((index, element) => {
+        const trElement = $(element);
+
+        const startTimeHref = trElement.find('td').eq(0).find('a').attr('href');
+        const startTimeIso = startTimeHref ? startTimeHref.split('=')[1].split('&')[0] : '';
+        const formattedStartTimeIso = `${startTimeIso.substring(0, 4)}-${startTimeIso.substring(4, 6)}-${startTimeIso.substring(6, 8)}T${startTimeIso.substring(9, 11)}:${startTimeIso.substring(11)}`;
+        
+        const contestLink = trElement.find('td').eq(1).find('a').attr('href');
+        const contestName = trElement.find('td').eq(1).text().replace('Ⓐ', '').replace('◉', '').replace('Ⓗ', '').trim();
+        const duration = trElement.find('td').eq(2).text().trim();
+        
+        const [hours, minutes] = duration.split(':');
+        const durationMinutes = Number(hours) * 60 + Number(minutes);
+        
+        const startTimeJST = new Date(formattedStartTimeIso);
+        const startTime = new Date(startTimeJST.getTime() - (3.5 * 60 * 60 * 1000)).getTime(); // JST is IST+3.5
+
+        const endTime = startTime + durationMinutes * 60 * 1000;
+        
+        contests.push({
+            site: PLATFORM.ATCODER,
+            title: contestName,
+            startTime,
+            endTime,
+            duration: durationMinutes,
+            url: ATCODER_BASE_URL + (contestLink ? contestLink : "")
+        });
+    });
+
+    return contests;
+}
+
 export const getAtcoderContests = async () => {
     try {
-        const contests: CONTEST_INTERFACE[] = [];
+        let contests: CONTEST_INTERFACE[] = [];
 
         const { data: markup } = await axios.get(ATCODER_CONTESTS_PAGE);
         const $ = cheerio.load(markup);
 
-        const tbody = $('#contest-table-upcoming').find('tbody');
+        const tBodyActive = $('#contest-table-action').find('tbody');;
+        contests = [...contests, ...parseTable($,tBodyActive)];
 
-        // Iterate over each tr element
-        tbody.find('tr').each((index, element) => {
-            const trElement = $(element);
-
-            const startTimeHref = trElement.find('td').eq(0).find('a').attr('href');
-            const startTimeIso = startTimeHref ? startTimeHref.split('=')[1].split('&')[0] : '';
-            const formattedStartTimeIso = `${startTimeIso.substring(0, 4)}-${startTimeIso.substring(4, 6)}-${startTimeIso.substring(6, 8)}T${startTimeIso.substring(9, 11)}:${startTimeIso.substring(11)}`;
-            
-            const contestLink = trElement.find('td').eq(1).find('a').attr('href');
-            const contestName = trElement.find('td').eq(1).text().replace('Ⓐ', '').replace('◉', '').replace('Ⓗ', '').trim();
-            const duration = trElement.find('td').eq(2).text().trim();
-            
-            const [hours, minutes] = duration.split(':');
-            const durationMinutes = Number(hours) * 60 + Number(minutes);
-            
-            const startTimeJST = new Date(formattedStartTimeIso);
-            const startTime = new Date(startTimeJST.getTime() - (3.5 * 60 * 60 * 1000)).getTime(); // JST is IST+3.5
-
-            console.log(new Date(formattedStartTimeIso));
-            const endTime = startTime + durationMinutes * 60 * 1000;
-            
-            contests.push({
-                site: PLATFORM.ATCODER,
-                startTime,
-                title: contestName,
-                duration: durationMinutes,
-                endTime,
-                url: ATCODER_BASE_URL + (contestLink ? contestLink : "")
-            });
-        });
+        const tbodyUpcoming = $('#contest-table-upcoming').find('tbody');
+        contests = [...contests, ...parseTable($,tbodyUpcoming)];        
 
         console.log('Fetched data from atcoder!', contests.length);
 
--- backend/src/routes/contest/contest.controller.ts
+++ backend/src/routes/contest/contest.controller.ts
@@ -3,12 +3,14 @@ import { CONTEST_INTERFACE, PLATFORM } from "../../types";
 import { atcoderContests, codechefContests, codeforcesContests, codingninjaContests, gfgContests, leetcodeContests, refreshCache } from "../../platforms/cache";
 
 const defaultArray = [PLATFORM.CODECHEF, PLATFORM.CODEFORCES, PLATFORM.LEETCODE];
+const DURATION_LIMIT = 15*24*60;
 
 export const UpcomingContestsController = async (
   req: Request,
   res: Response
 ) => {
   let platforms: string[];
+  const includedPlatorms: string[] = [];
   try {
     platforms = JSON.parse(req.query.platforms as string);
     if (!platforms || !Array.isArray(platforms))
@@ -21,18 +23,32 @@ export const UpcomingContestsController = async (
 
   await refreshCache();
 
-  if (platforms.includes(PLATFORM.CODECHEF))
+  if (platforms.includes(PLATFORM.CODECHEF)){
     contests = [...contests,...codechefContests];
-  if (platforms.includes(PLATFORM.LEETCODE))
+    includedPlatorms.push(PLATFORM.CODECHEF);
+  }
+  if (platforms.includes(PLATFORM.LEETCODE)){
     contests = [...contests,...leetcodeContests];
-  if (platforms.includes(PLATFORM.CODEFORCES))
+    includedPlatorms.push(PLATFORM.LEETCODE);
+  }
+  if (platforms.includes(PLATFORM.CODEFORCES)){
     contests = [...contests,...codeforcesContests];
-  if (platforms.includes(PLATFORM.GEEKSFORGEEKS))
+    includedPlatorms.push(PLATFORM.CODEFORCES);
+  }
+  if (platforms.includes(PLATFORM.GEEKSFORGEEKS)){
     contests = [...contests,...gfgContests];
-  if (platforms.includes(PLATFORM.ATCODER))
+    includedPlatorms.push(PLATFORM.GEEKSFORGEEKS);
+  }
+  if (platforms.includes(PLATFORM.ATCODER)){
     contests = [...contests,...atcoderContests];
-  if (platforms.includes(PLATFORM.CODINGNINJAS))
+    includedPlatorms.push(PLATFORM.ATCODER);
+  }
+  if (platforms.includes(PLATFORM.CODINGNINJAS)){
     contests = [...contests,...codingninjaContests];
+    includedPlatorms.push(PLATFORM.CODINGNINJAS);
+  }
+
+  contests = contests.filter((contest) => (contest.duration <= DURATION_LIMIT));
 
   contests.sort(
     (contest1, contest2) =>
@@ -40,7 +56,7 @@ export const UpcomingContestsController = async (
   );
 
   res.status(200).json({
-    platforms,
+    platforms: includedPlatorms,
     upcoming_contests: contests,
   });
 };
